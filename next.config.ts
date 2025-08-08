cadadeccf01c05d8faa3bdce42fa705726caa758
Nathan Covey <156732802+nathancovey@users.noreply.github.com>
2025-08-08 17:30:36 -0500

    allow outrank cdn

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,17 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'i.ytimg.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'img.youtube.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.outrank.so',
         pathname: '/**',
       },
     ],
